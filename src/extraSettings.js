dfda307bca0e0181213f08ed1beb502d03e813f6
eno-cao <78261683+eno-cao@users.noreply.github.com>
2021-01-30 21:16:35 +0800

    Add files via upload

---
+++ src/extraSettings.js
@@ -0,0 +1,23 @@
+// 额外分数：修改数字
+let extraScore = 10;
+
+// 无敌模式：true 改为 false
+let wuDi = true;
+
+// 第一个水果：修改数字为 0-10
+let firstFruit = 0;
+
+// 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
+let fruitQTan = false;
+
+// 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
+let fruitSlowDown = false;
+
+// 广告链接：false 或为空字符串表示不会跳转到广告
+let adLink = ' ';
+
+// 修改网页标题：将 "合成大西瓜" 进行替换
+document.getElementsByTagName("title")[0].innerText = '合成秦时明月人物';
+
+// 开启弹窗，暂不支持
+let selectModal = false;
